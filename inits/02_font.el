c68b3db69f1289a6324b2fd9f401d117cf4146d2
K.Se <kse@win7>
2013-10-31 08:42:06 +0900

    tweak font in dos

--- inits/02_font.el
+++ inits/02_font.el
@@ -2,7 +2,7 @@
   (cond ((eq ws 'w32)
          (set-face-attribute 'default nil
                              :family "Ricty Diminished"  ;; 英数
-                             :height 100)
+                             :height 140)
          (set-fontset-font nil 'japanese-jisx0208 (font-spec :family "Ricty Diminished")))  ;; 日本語
         ((eq ws 'ns)
          (set-face-attribute 'default nil
