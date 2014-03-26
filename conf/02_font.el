c990341cc858875591b713952fc4a4cdcbf96184
kse <kse@Jonathan.local>
2014-03-26 22:12:33 +0900

    tweak for Mac OS

--- conf/02_font.el
+++ conf/02_font.el
@@ -4,7 +4,7 @@
                              :family "Ricty Diminished"  ;; 英数
                              :height 140)
          (set-fontset-font nil 'japanese-jisx0208 (font-spec :family "Ricty Diminished")))  ;; 日本語
-        ((eq system-type 'ns)
+        ((eq system-type 'darwin)
          (set-face-attribute 'default nil
                              :family "Ricty Diminished"  ;; 英数
                              :height 160)
