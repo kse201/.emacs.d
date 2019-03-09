bd2b7ef10a9025cbb34468f886ca337bdd3d155f
kse201 <kse.201@gmail.com>
2019-03-09 15:41:46 +0900

    refactor: modify config path

--- init.el
+++ init.el
@@ -1,2 +1,2 @@
-(defvar my-config-dir (concat user-emacs-directory "config/"))
+(defvar my-config-dir (concat user-emacs-directory "conf.org.d"))
 (org-babel-load-file (expand-file-name "init.org" my-config-dir))
